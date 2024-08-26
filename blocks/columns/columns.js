c294325233b736367062808a1858929926f12ee5
Timo Leenen <leenen@adobe.com>
2024-08-26 11:07:11 +0200

    Added dynamic column support for Columns component

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,6 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  // Set number of columns as a css style variable, so we can calculate max item width in CSS
+  block.style.setProperty(`--number-of-columns`, `${cols.length}`);
 }
